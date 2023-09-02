5f607b712dcca49b21d56b338a32d45e6332f096
Alexander Pogosyan <aleksandrpogosyan76@gmail.com>
2023-09-02 19:44:24 +0400

    добавил  члены класса School

---
+++ OnlineSchool/School.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineSchool
+{
+    internal class School
+    {
+        private string Name { get; }
+        private List<Student> student = new List<Student>();
+
+        public School(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
