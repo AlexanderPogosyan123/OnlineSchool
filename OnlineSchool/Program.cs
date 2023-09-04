3a24edd615c2a090bfea645256a4e8b825e151bb
Alexander Pogosyan <aleksandrpogosyan76@gmail.com>
2023-09-04 13:30:31 +0400

    добавил список и ввод номера из списка

--- OnlineSchool/Program.cs
+++ OnlineSchool/Program.cs
@@ -4,6 +4,22 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Здраствуйте! Введите наименование школы: ");
+            string schoolName = Console.ReadLine();
+            School school = new School(schoolName);
+            Console.WriteLine($"Школа {school.Name} успешна создана!");
+            Console.WriteLine();
+
+            Console.WriteLine("Список Команд для Администрирования: ");
+
+            school.ShowListOptions();
+            
+            Console.WriteLine("Выберите действие из списка(введите просто цифру): ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            school.CheckAndCorrectFormatIndexOfList( ref num);
+
+            
 
         }
     }
