ffb45b6b1404a2d1e5426143b6d314e58ee04cf8
Alexander Pogosyan <aleksandrpogosyan76@gmail.com>
2023-09-04 13:59:51 +0400

    добавил Switch case для взаимодействия с юзером

--- OnlineSchool/Program.cs
+++ OnlineSchool/Program.cs
@@ -13,14 +13,45 @@
             Console.WriteLine("Список Команд для Администрирования: ");
 
             school.ShowListOptions();
-            
-            Console.WriteLine("Выберите действие из списка(введите просто цифру): ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            
-            school.CheckAndCorrectFormatIndexOfList( ref num);
 
-            
 
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Выберите действие из списка(введите просто цифру): ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                school.CheckAndCorrectFormatIndexOfList(ref num);
+                int c = 0;
+                switch (num)
+                {
+                    case 1:
+                        c++;
+                        school.AddNewStudent();
+                        break;
+                    case 2:
+                        school.PrintAllStudents();
+                        break;
+                    case 3:
+                        if (c == 0)
+                        {
+                            Console.WriteLine("В школе пока нет студентов, чтобы кого-то удалить");
+                        }
+                        else
+                        {
+                            school.DeleteStudentFromSchool();
+                        }
+                            break;
+                    case 4:
+                        school.Exit();
+                        break;
+                    case 5:
+                        school.ShowListOptions();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
